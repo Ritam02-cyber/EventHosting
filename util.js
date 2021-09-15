ee234dd70b7971eba94bec07e4c1821a5446d72c
Sachin <sksachin7z2@gmail.com>
2021-09-15 14:36:01 +0530

    almost done suggestion needed

--- util.js
+++ util.js
@@ -12,3 +12,32 @@ function w3_open() {
 function w3_close() {
     document.getElementById("mySidebar").style.width = "0vw";
 }
+function changeList() {
+    var sg=document.getElementById("events").value;
+    
+    if(sg=="Club_events"){
+        document.getElementById("All_events").style.display="none";
+        document.getElementById("Club_events").style.display="flex";
+        document.getElementById("society").style.display="none";
+        document.getElementById("other").style.display="none";
+    }
+    if(sg=="All_events"){
+        document.getElementById("All_events").style.display="flex";
+        document.getElementById("Club_events").style.display="none";
+        document.getElementById("society").style.display="none";
+        document.getElementById("other").style.display="none";
+    }
+    if(sg=="Society_events"){
+        document.getElementById("All_events").style.display="none";
+        document.getElementById("Club_events").style.display="none";
+        document.getElementById("society").style.display="flex";
+        document.getElementById("other").style.display="none";
+    }
+    if(sg=="Other_events"){
+        document.getElementById("All_events").style.display="none";
+        document.getElementById("Club_events").style.display="none";
+        document.getElementById("society").style.display="none";
+        document.getElementById("other").style.display="flex";
+    }
+    
+}
